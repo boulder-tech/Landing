894865d10ed90851f20485cd4655cce6599e9fa2
Arotu <arotuoficial@gmail.com>
2023-11-30 06:15:55 -0300

    All assets added and other utils

---
+++ src/utils/benefit-data.ts
@@ -0,0 +1,41 @@
+import RealWorldIcon from '../../public/assets/real-world.svg';
+import TransparentIcon from '../../public/assets/transparent.svg';
+import LowEntryIcon from '../../public/assets/low-barrier.svg';
+import ProfessionalIcon from '../../public/assets/professional.svg';
+import ReduceIcon from '../../public/assets/reduce-costs.svg';
+import InstantIcon from '../../public/assets/instant.svg';
+
+const benefitData = [
+    {
+        title: 'Real World',
+        body: 'Tangible assets that exist in the physical world, brought on chain.',
+        icon: RealWorldIcon,
+    },
+    {
+        title: 'Transparent',
+        body: 'It lowers costs by removing middlemen like lawyers, brokers, banks, etc.',
+        icon: TransparentIcon,
+    },
+    {
+        title: 'Low entry barrier',
+        body: 'Accessible and simple, lowering the entry barrier and creating more liquidity',
+        icon: LowEntryIcon,
+    },
+    {
+        title: 'Professional',
+        body: 'Token issuer regulated by the British Virgin Islands Financial Services Commission.',
+        icon: ProfessionalIcon,
+    },
+    {
+        title: 'Reduce costs',
+        body: 'By removing middlemen like lawyers, brokers and banks.',
+        icon: ReduceIcon,
+    },
+    {
+        title: 'Instant',
+        body: 'Mint and redeem tokens on-chain, instantly, and on a 24/7 basis.',
+        icon: InstantIcon,
+    },
+];
+
+export default benefitData;
