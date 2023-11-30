8263665850161b62410a4bf6cb5eadfbda4e3956
Arotu <arotuoficial@gmail.com>
2023-11-30 15:41:22 -0300

    Tailwind config set-up

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,36 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      fontFamily: {
-        "atyp-display": ['var(--font-atyp-display)'],
-        "atyp-text": ['var(--font-atyp-text)'],
-      },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      extend: {
+        fontFamily: {
+          "atyp-display": ["var(--font-atyp-display)"],
+          "atyp-text": ["var(--font-atyp-text)"],
+        },
+        colors: {
+          foreground: {
+            heading: "var(--color-foreground-heading)",
+            leading: "var(--color-foreground-leading)",
+            dark: "var(--color-foreground-dark)",
+          },
+          background: {
+            DEFAULT: "var(--color-background)",
+          },
+          grey:{
+            "100": "var(--color-grey-100)",
+            "200": "var(--color-grey-200)",
+            "300": "var(--color-grey-300)",
+          }
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
