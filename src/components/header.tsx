831bb3ff7e3acb26a954537caa1c93208e418f0d
Arotu <arotuoficial@gmail.com>
2023-11-30 15:58:32 -0300

    Minor changes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,4 @@
 const Header = () => {
-  return <div>Header</div>;
+  return <header className="w-full fixed top-0 z-50 py-3 bg-[rgba(1, 3, 18, 0.01)] border-b border-grey-200 backdrop-blur-lg">Header</header>
 };
 export default Header;
