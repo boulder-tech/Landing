776c1e9158201e50d33cd33832292be629f18e12
Arotu <arotuoficial@gmail.com>
2023-11-30 15:50:13 -0300

    Earth completed

---
+++ src/components/earth.tsx
@@ -0,0 +1,36 @@
+import { Canvas, useLoader, useFrame } from "@react-three/fiber";
+import { TextureLoader } from "three/src/loaders/TextureLoader";
+import { useRef } from "react";
+
+function EarthMesh() {
+  const [color, normal, aoMap] = useLoader(TextureLoader, [
+    "/assets/color.jpg",
+    "/assets/normal.png",
+    "/assets/occlusion.jpg",
+  ]);
+
+  const mesh = useRef();
+
+  useFrame(() => {
+    // @ts-ignore
+    mesh.current.rotation.y += 0.001;
+  });
+
+  return (
+    <mesh scale={2.5} ref={mesh}>
+      <sphereGeometry args={[1, 64, 64]} />
+      <meshStandardMaterial map={color} normalMap={normal} aoMap={aoMap} />
+    </mesh>
+  );
+}
+
+export default function EarthModel() {
+
+  return (
+    <Canvas>
+      <ambientLight intensity={0.1} />
+      <directionalLight intensity={3.5} position={[2, 0, -0.25]} />
+      <EarthMesh />
+    </Canvas>
+  );
+}
