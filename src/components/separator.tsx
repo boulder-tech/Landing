91aa3a86d0c36e4ad5d82d5662de96d341e7d61c
Arotu <arotuoficial@gmail.com>
2023-12-01 19:40:13 -0300

    Separator component finished

--- src/components/separator.tsx
+++ src/components/separator.tsx
@@ -6,6 +6,6 @@ type separatorProps = {
 
 const Separator:React.FC<separatorProps> = ({orientation}) => {
     
-    return <div className={clsx(orientation === "horizontal" ? "w-full h-px" : "w-px h-full", "borde")}></div>
+    return <div className={clsx(orientation === "horizontal" ? "w-full h-px" : "w-px h-full", "bg-gradient-to-r from-transparent via-gray-300 to-transparent")}></div>
 }
 export default Separator;
