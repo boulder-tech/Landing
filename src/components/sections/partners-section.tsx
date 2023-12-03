5e4333c360bfcfad81990faff9b2e140591eaf88
Arotu <arotuoficial@gmail.com>
2023-12-03 14:44:19 -0300

    Minor changes

--- src/components/sections/partners-section.tsx
+++ src/components/sections/partners-section.tsx
@@ -1,4 +1,17 @@
 const PartnersSection = () => {
-  return <section>Partners Section</section>
+  return (
+    <section className="w-full flex items-center justify-center relative py-16 text-center flex-col gap-16">
+      <div className="px-8 flex flex-col gap-4 items-center justify-center">
+        <h2 className="font-atyp-display text-[32px] -tracking-[0.72px] text-transparent bg-clip-text bg-gradient-to-t from-[#FAFBFF] to-[#B3B9CB]">
+          Elevate your Investments with Custodial Brilliance
+        </h2>
+        <p className="text-foreground-leading text-base w-full max-w-3xl">
+          Explore unparalleled asset security with our commitment to thorough
+          segregation, overseen by regulated and insured qualified custodians
+          dedicated to safeguarding your investments.
+        </p>
+      </div>
+    </section>
+  );
 };
 export default PartnersSection;
