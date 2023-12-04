601a3e8745096926502f446f911f64cee8ea36bf
Arotu <arotuoficial@gmail.com>
2023-12-04 07:34:49 -0300

    Hamburger menu

--- src/components/buttons/ghost-button.tsx
+++ src/components/buttons/ghost-button.tsx
@@ -1,6 +1,22 @@
-const GhostButton = ({ children }: { children: React.ReactNode }) => {
+import clsx from "clsx";
+
+type ghostButtonProps = {
+  children: React.ReactNode;
+  onClick?: () => void;
+  classes?: string;
+  iconBtn?: boolean;
+};
+
+const GhostButton = ({ children, onClick, classes, iconBtn }: ghostButtonProps) => {
   return (
-    <button className="transition-all font-medium duration-200 hover:bg-gray-200 hover:border-gray-300 bg-gray-100 border border-gray-200 rounded-full px-4 text-sm h-10 text-foreground-heading flex items-center justify-center">
+    <button
+      onClick={onClick}
+      className={clsx(
+        "transition-all font-medium duration-200 hover:bg-gray-200 hover:border-gray-300 bg-gray-100 border border-gray-200 rounded-full px-4 text-sm h-10 text-foreground-heading flex items-center justify-center",
+        iconBtn && "w-10",
+        classes,
+      )}
+    >
       {children}
     </button>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,42 +1,50 @@
+"use client";
 import Link from "next/link";
 import Imagotype from "./imagotype";
 import GhostButton from "./buttons/ghost-button";
+import Image from "next/image";
+import clsx from "clsx";
+import Hamburger from "../../public/icons/hamburger.svg";
 
 const Header = () => {
   return (
-    <header className="w-full fixed top-0 z-50 py-3 bg-[rgba(1, 3, 18, 0.01)] border-b border-grey-200 backdrop-blur-lg">
-      <div className="flex items-center justify-between 2xl:px-[300px] lg:px-[150px] md:px-[75px] px-[20px]">
-        <div className="flex gap-4 items-center justify-center divide-border-grey-200 divide-x-[1px]">
-          <Link href={'/'}>
-            <Imagotype />
-          </Link>
-          <div className="pl-4 text-boulder-text sm:flex hidden text-sm gap-4 font-normal -tracking-[0.28px]">
-            <Link
-              className="cursor-pointer font-medium hover:opacity-50 transition-all duration-200"
-              href={"/about"}
-            >
-              About us
+    <>
+      <header className="w-full fixed h-[65px] top-0 z-50 py-3 bg-[rgba(1, 3, 18, 0.01)] border-b border-grey-200 backdrop-blur-lg">
+        <div className="flex items-center justify-between 2xl:px-[300px] lg:px-[150px] md:px-[75px] px-[20px]">
+          <div className="flex gap-4 items-center justify-center divide-border-grey-200 divide-x-[1px]">
+            <Link href={"/"}>
+              <Imagotype />
             </Link>
-            <Link
-              className="cursor-pointer font-medium hover:opacity-50 transition-all duration-200"
-              href={"/contact"}
-            >
-              Contact
-            </Link>
-            <Link
-              className="cursor-pointer font-medium hover:opacity-50 transition-all duration-200"
-              href={"https://docs.bouldertech.fi"}
-              target="_blank"
-            >
-              Documentation ↗
-            </Link>
+            <div className="pl-4 text-boulder-text sm:flex hidden text-sm gap-4 font-normal -tracking-[0.28px]">
+              <Link
+                className="cursor-pointer font-medium hover:opacity-50 transition-all duration-200"
+                href={"/about"}
+              >
+                About us
+              </Link>
+              <Link
+                className="cursor-pointer font-medium hover:opacity-50 transition-all duration-200"
+                href={"/contact"}
+              >
+                Contact
+              </Link>
+              <Link
+                className="cursor-pointer font-medium hover:opacity-50 transition-all duration-200"
+                href={"https://docs.bouldertech.fi"}
+                target="_blank"
+              >
+                Documentation ↗
+              </Link>
+            </div>
           </div>
+          <GhostButton iconBtn onClick={()=>console.log("ja")} classes="sm:hidden"><Image src={Hamburger} alt="Hamburger menu" width={14} height={14} className="w-4 h-4 absolute"/></GhostButton>
+          <Link href={"/#waitlist"} className="hidden sm:block">
+            <GhostButton>Join waitlist →</GhostButton>
+          </Link>
         </div>
-        <Link href={"/#waitlist"}>
-          <GhostButton>Join waitlist →</GhostButton>
-        </Link>
-      </div>
-    </header>
+      </header>
+      <div className="fixed top-[65px] w-full h-[calc(100vh-65px)] bg-[rgba(1, 3, 18, 0.01)] backdrop-blur-lg z-50 flex items-center justify-center"></div>
+    </>
   );
 };
 export default Header;
