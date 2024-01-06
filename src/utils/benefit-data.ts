3d83d7c8b92678f2581d2cc188ff2b4af3006382
Leonel <leonel@bouldertech.fi>
2024-01-05 21:38:18 -0500

    fix typo at reduce costs description

--- src/utils/benefit-data.ts
+++ src/utils/benefit-data.ts
@@ -8,32 +8,38 @@ import InstantIcon from '../../public/assets/Instant.svg';
 const benefitData = [
     {
         title: 'Real World',
-        description: 'Tangible assets that exist in the physical world, brought on chain.',
+        description:
+            'Tangible assets that exist in the physical world, brought on chain.',
         icon: RealWorldIcon,
     },
     {
         title: 'Transparent',
-        description: 'It lowers costs by removing middlemen like lawyers, brokers, banks, etc.',
+        description:
+            'It lowers costs by removing middlemen like lawyers, brokers, banks, etc.',
         icon: TransparentIcon,
     },
     {
         title: 'Low entry barrier',
-        description: 'Accessible and simple, lowering the entry barrier and creating more liquidity',
+        description:
+            'Accessible and simple, lowering the entry barrier and creating more liquidity',
         icon: LowEntryIcon,
     },
     {
         title: 'Professional',
-        description: 'Token issuer regulated by the Bermuda Monetary Authority.',
+        description:
+            'Token issuer regulated by the Bermuda Monetary Authority.',
         icon: ProfessionalIcon,
     },
     {
         title: 'Reduce costs',
-        description: 'By removing middlemen likes lawyers, brokers and banks.',
+        description:
+            'By removing middlemen such as lawyers, brokers, and banks.',
         icon: ReduceIcon,
     },
     {
         title: 'Instant',
-        description: 'Mint and redeem tokens on-chain, instantly, and on a 24/7 basis.',
+        description:
+            'Mint and redeem tokens on-chain, instantly, and on a 24/7 basis.',
         icon: InstantIcon,
     },
 ];
