cfaf64ff933ead94a51b6e35996d292d7b236881
Benzagon <gonbenzaquen@gmail.com>
2024-01-06 23:30:34 -0300

    fix: issue with about responsive wrapping
    Issue when shrinking about section, cards not wrapping

--- src/components/sections/meetTheTeam-section.tsx
+++ src/components/sections/meetTheTeam-section.tsx
@@ -2,7 +2,7 @@ import PersonCard from '../cards/person-card';
 
 const MeetTheTeam = () => {
     return (
-        <div className="h-full flex flex-col items-center justify-center gap-10 relative 2xl:px-[300px] lg:px-[150px] md:px-[75px] px-[20px]">
+        <div className="h-full flex flex-col items-center justify-center gap-10 relative 2xl:px-[200px] lg:px-[150px] md:px-[75px] px-[20px]">
             <div className="max-w-[52rem] flex flex-col items-center gap-8 text-center relative">
                 <h1 className="text-foreground-heading sm:text-[60px] text-[34px] font-atyp-display font-medium leading-[120%] z-10">
                     Meet the team
@@ -12,7 +12,7 @@ const MeetTheTeam = () => {
                     finance
                 </h3>
             </div>
-            <div className="flex sm:flex-row flex-col gap-11">
+            <div className="flex flex-wrap justify-center gap-8">
                 <PersonCard
                     name="Rodrigo Benzaquen"
                     role="CEO"
