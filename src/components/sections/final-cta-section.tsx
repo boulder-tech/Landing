609cc9d0ce5b8de77a3e537ff6ef4074d506e35f
Benzagon <gonbenzaquen@gmail.com>
2024-01-28 20:44:29 -0300

    fix: set finalcta image to bg
    Doing so will fix sizing issues

--- src/components/sections/final-cta-section.tsx
+++ src/components/sections/final-cta-section.tsx
@@ -1,18 +1,10 @@
-import BrandButton from "../buttons/brand-button";
 import Image from "next/image";
 import MountainVector from "../../../public/assets/mountain-vector.svg";
 import Waitlist from "../buttons/waitlist-input";
 
 const FinalCtaSection = () => {
   return (
-    <section className="flex flex-col items-center justify-center relative px-8 sm:mt-56 mt-28 mb-44 w-full gap-10">
-      <Image
-        src={MountainVector}
-        alt="Background asset"
-        className="pointer-events-none absolute -bottom-48"
-        width={1920}
-        height={1080}
-      />
+    <section className="flex bg-[url(/assets/mountain-vector.png)] bg-cover bg-center bg-no-repeat flex-col items-center justify-center relative px-8 sm:pt-56 pt-28 pb-44 w-full gap-10">
       <h1 className="z-10 font-atyp-display sm:text-6xl text-[35px] font-medium max-w-3xl text-center">
         Look into the future with{" "}
         <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#00B2FF] to-[#245BFF] whitespace-nowrap">
