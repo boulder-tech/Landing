215e7c5096c9bf1d77feeee1cf864b048b055dce
Benzagon <gonbenzaquen@gmail.com>
2024-02-01 13:47:40 -0300

    fix: type issue with date

--- lib/getFormattedDate.tsx
+++ lib/getFormattedDate.tsx
@@ -1,3 +1,3 @@
-export default function getFormattedDate(dateString: string): string {
+export default function getFormattedDate(dateString: Date): string {
     return new Intl.DateTimeFormat('en-US', { dateStyle: 'long' }).format(new Date(dateString))
 }
