2d9007df40a859ec4ec26505d598ce6e08c394e2
Leonel <leonel@bouldertech.fi>
2024-02-20 16:51:29 -0500

    change Professional description

--- src/utils/benefit-data.ts
+++ src/utils/benefit-data.ts
@@ -6,42 +6,41 @@ import ReduceIcon from '../../public/assets/reduce-costs.svg';
 import InstantIcon from '../../public/assets/Instant.svg';
 
 const benefitData = [
-    {
-        title: 'Real World',
-        description:
-            'Tangible assets that exist in the physical world, brought on chain.',
-        icon: RealWorldIcon,
-    },
-    {
-        title: 'Transparent',
-        description:
-            'It lowers costs by removing middlemen like lawyers, brokers, banks, etc.',
-        icon: TransparentIcon,
-    },
-    {
-        title: 'Low entry barrier',
-        description:
-            'Accessible and simple, lowering the entry barrier and creating more liquidity',
-        icon: LowEntryIcon,
-    },
-    {
-        title: 'Professional',
-        description:
-            'Token issuer regulated by the Bermuda Monetary Authority.',
-        icon: ProfessionalIcon,
-    },
-    {
-        title: 'Reduce costs',
-        description:
-            'By removing middlemen such as lawyers, brokers, and banks.',
-        icon: ReduceIcon,
-    },
-    {
-        title: 'Instant',
-        description:
-            'Mint and redeem tokens on-chain, instantly, and on a 24/7 basis.',
-        icon: InstantIcon,
-    },
+  {
+    title: 'Real World',
+    description:
+      'Tangible assets that exist in the physical world, brought on chain.',
+    icon: RealWorldIcon,
+  },
+  {
+    title: 'Transparent',
+    description:
+      'It lowers costs by removing middlemen like lawyers, brokers, banks, etc.',
+    icon: TransparentIcon,
+  },
+  {
+    title: 'Low entry barrier',
+    description:
+      'Accessible and simple, lowering the entry barrier and creating more liquidity',
+    icon: LowEntryIcon,
+  },
+  {
+    title: 'Professional',
+    description:
+      'Token issuer for which a licensing application has been submitted to the Bermuda Monetary Authority.',
+    icon: ProfessionalIcon,
+  },
+  {
+    title: 'Reduce costs',
+    description: 'By removing middlemen such as lawyers, brokers, and banks.',
+    icon: ReduceIcon,
+  },
+  {
+    title: 'Instant',
+    description:
+      'Mint and redeem tokens on-chain, instantly, and on a 24/7 basis.',
+    icon: InstantIcon,
+  },
 ];
 
 export default benefitData;
