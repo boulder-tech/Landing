e87d9474bb0ed79676e88a38bc7572f9976152e0
Benzagon <gonbenzaquen@gmail.com>
2024-08-05 19:31:35 -0300

    fix: remove link to app

--- src/components/cards/token-card.tsx
+++ src/components/cards/token-card.tsx
@@ -12,7 +12,8 @@ interface Props {
 
 const TokenCard = ({className, name, ticker, icon, bgEffect}: Props) => {
   return (
-    <Link target='__blank' href={`https://app.bouldertech.fi/${ticker.toLowerCase()}`} className={`flex flex-col justify-between items-start  md:w-[27rem] md:h-[12rem] w-[20rem] h-[10rem] p-6 rounded-xl border border-[#fafbff12] bg-card-dark relative overflow-hidden group ${className}`}>
+    // <Link target='__blank' href={`https://app.bouldertech.fi/${ticker.toLowerCase()}`} className={`flex flex-col justify-between items-start  md:w-[27rem] md:h-[12rem] w-[20rem] h-[10rem] p-6 rounded-xl border border-[#fafbff12] bg-card-dark relative overflow-hidden group ${className}`}>
+    <div className={`flex flex-col justify-between items-start md:w-[27rem] md:h-[12rem] sm:w-[22rem] w-full h-[10rem] p-6 rounded-xl border border-[#fafbff12] bg-card-dark relative overflow-hidden group ${className}`}>
         <div className='flex flex-col gap-3 z-10'>
             <h3 className='font-atyp-display md:text-2xl text-xl'>{name}</h3>
             <div className='flex gap-2 items-center'>
@@ -20,15 +21,14 @@ const TokenCard = ({className, name, ticker, icon, bgEffect}: Props) => {
                 <p className='text-foreground-leading md:text-xl text-lg'>{ticker}</p>
             </div>
         </div>
-        <div className='w-full h-fit flex justify-end items-center z-10'>
+        {/* <div className='w-full h-fit flex justify-end items-center z-10'>
             <p className='px-4 text-accent font-medium'>Invest →</p>
-        </div>
+        </div> */}
         <div className='absolute -right-16 -top-20'>
-            <div className='w-96 h-56'>
-                <Image quality={80} src={bgEffect} aria-hidden alt='' unselectable='on' width={0} height={0} className='w-full h-auto pointer-events-none select-none'></Image>
-            </div>
+            <Image quality={80} src={bgEffect} aria-hidden alt='' unselectable='on' width={384} height={224} className='pointer-events-none select-none'></Image>
         </div>
-    </Link>
+    {/* </Link> */}
+    </div>
   )
 }
 
--- src/components/sections/stocks-section.tsx
+++ src/components/sections/stocks-section.tsx
@@ -15,7 +15,7 @@ const StocksSection = () => {
             <p className='md:hidden flex text-foreground-leading leading-normal text-base 2xl:text-lg xl:max-w-none sm:max-w-[38rem]'>{"Each token is backed by financial assets, providing a secure and compliant way to access to TradFi via crypto."}</p>
             <p className='md:flex hidden text-foreground-leading leading-normal text-base 2xl:text-lg xl:max-w-none sm:max-w-[38rem]'>{"Each token is backed by actual financial assets, offering a secure and compliant way to diversify your portfolio. This backing ensures real-world value and stability, making your investment both reliable and robust."}</p>
         </div>
-        <div className='relative flex flex-col xl:items-end items-center xl:justify-end justify-center w-fit xl:gap-0 gap-4'>
+        <div className='relative flex flex-col xl:items-end items-center xl:justify-end justify-center xl:gap-0 gap-4 w-full'>
             <TokenCard className='z-20' name='S&P 500 UCITS ETF' ticker='CSPX' icon={cspxIcon} bgEffect={cspxEffect}></TokenCard>
             <TokenCard className='z-30 xl:mr-32 xl:flex absolute top-auto xl:-top-6' name='Invesco QQQ Trust Series 1' ticker='QQQ' icon={qqqIcon} bgEffect={qqqEffect}></TokenCard>
             <TokenCard className='z-40 xl:mr-64 xl:flex absolute top-auto xl:-top-12' name='Treasury Bond 0-1yr UCITS' ticker='IB01' icon={ib01Icon} bgEffect={ib01Effect}></TokenCard>
