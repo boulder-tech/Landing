3826dd76b0602a6b72fe4c5a04932750676dd6ca
Benzagon <gonbenzaquen@gmail.com>
2024-08-05 16:28:21 -0300

    feat: mobile mediaquery for grant section

--- src/components/sections/grant-support-section.tsx
+++ src/components/sections/grant-support-section.tsx
@@ -14,11 +14,11 @@ const GrantSection = () => {
           We are proud recipients of the Arbitrum Foundation&#39;s Grants program.
         </p>
       </div>
-      <div className="px-8 sm:gap-8 gap-4 w-fit flex items-center justify-center z-20">
-        <div className="w-fit h-fit animate-hover sm:mt-16 mt-8">
-          <Image quality={100} src={coinAsset} alt="Arbitrum 3D Coin" width={207} height={207} className="sm:w-auto w-24 select-none"></Image>
+      <div className="px-8 sm:gap-8 gap-4 w-fit flex sm:flex-row flex-col items-center justify-center z-20">
+        <div className="w-fit h-fit animate-hover sm:mt-16 mt-2">
+          <Image quality={100} src={coinAsset} alt="Arbitrum 3D Coin" width={207} height={207} className="sm:w-auto w-32 select-none"></Image>
         </div>
-        <Image src={textAsset} alt="Arbitrum Text" width={392.5} height={68} className="sm:w-auto w-48"></Image>
+        <Image src={textAsset} alt="Arbitrum Text" width={392.5} height={68} className="sm:w-auto w-48 sm:flex hidden"></Image>
       </div>
       <span
         className="absolute pointer-events-none z-0 top-[37%] left-[25%] w-[33rem] h-[23rem] blur-[275px] rounded-full bg-accent"
