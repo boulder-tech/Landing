7c88a739528d0d36a7e20c2bedebbf3a0152cd35
Gonzalo Benzaquen <91753390+Benzagon@users.noreply.github.com>
2024-08-07 21:39:24 -0300

    Update header.tsx
    Add missing "↗"

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -46,7 +46,7 @@ const Header = () => {
                 href={"https://docs.bouldertech.fi"}
                 target="_blank"
               >
-                Documentation 
+                Documentation ↗
               </Link>
             </div>
           </div>
