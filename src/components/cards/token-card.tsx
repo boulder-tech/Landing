31be7e2d290661c19439c5a85dbfe59c85392997
Benzagon <gonbenzaquen@gmail.com>
2024-12-16 17:25:09 -0300

    feat: links and clicable buttons

--- src/components/cards/token-card.tsx
+++ src/components/cards/token-card.tsx
@@ -13,15 +13,15 @@ interface Props {
 
 const TokenCard = ({className, ticker, icon, url, bgColor, isLight}: Props) => {
     return (
-        <div className={`flex items-center justify-center h-20 sm:h-36 w-full sm:w-40 rounded-lg group overflow-hidden relative cursor-default ${bgColor} ${!isLight ? 'text-text-white' : 'text-text-black'}`}>
+        <Link href={url} target='_blank' className={`flex items-center justify-center h-20 sm:h-36 w-full sm:w-40 rounded-lg group overflow-hidden relative cursor-pointer ${bgColor} ${!isLight ? 'text-text-white' : 'text-text-black'}`}>
             <div className='flex gap-2 items-center justify-center group-hover:opacity-0 duration-100'>
                 <Image src={icon} width={25} height={25} alt={ticker + " icon"} className='md:w-fit w-8'></Image>
                 <p className='md:text-2xl font-medium text-lg'>{ticker}</p>
             </div>
-            <div className='absolute -bottom-5 group-hover:bottom-[3.75rem] transition-all duration-200 ease-in-out'>
-                <Link href={url} className='font-medium'>More Info ↗</Link>
+            <div className='absolute -bottom-5 group-hover:bottom-[3.75rem] transition-all duration-300 ease-in-out'>
+                <p className='font-medium'>More Info ↗</p>
             </div>
-        </div>
+        </Link>
     )
 }
 
