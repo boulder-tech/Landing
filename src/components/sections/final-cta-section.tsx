65ba933bcaacee185507c8069d2cfc73c534251b
Nicolas Taddeo <94655836+colotaddeo@users.noreply.github.com>
2025-03-27 10:09:01 -0300

    fix: final CTA section
    text changed

--- src/components/sections/final-cta-section.tsx
+++ src/components/sections/final-cta-section.tsx
@@ -3,8 +3,8 @@ import { BrandLink } from "../buttons/brand-button";
 const FinalCtaSection = () => {
   return (
     <section className="flex bg-[url(/assets/mountain-vector.png)] bg-cover bg-center bg-no-repeat flex-col items-center justify-center relative px-8 sm:pt-60 pt-28 pb-44 w-full gap-10">
-      <h1 className="z-10 font-atyp-display sm:text-6xl text-[35px] font-medium max-w-3xl text-center">
-        Look into the future with{" "}
+      <h1 className="z-10 font-atyp-display sm:text-6xl text-[35px] font-medium max-w-4xl text-center">
+      Dive into Hyper-Tokenization with {" "}
         <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#00B2FF] to-[#245BFF] whitespace-nowrap">
           Boulder Tech
         </span>
