766cb28d8c4f1181b781aae9b4c5da982c59be6c
Nicolas Taddeo <94655836+colotaddeo@users.noreply.github.com>
2025-03-27 00:00:16 -0300

    fix: Stocks section
    Title and subtitle text changed. Ticker and asset added.

--- src/components/cards/token-card.tsx
+++ src/components/cards/token-card.tsx
@@ -1,9 +1,11 @@
+import { assert } from 'console'
 import Image, { StaticImageData } from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 interface Props {
     className?: string,
+    asset: string,
     ticker: string,
     icon: StaticImageData,
     url: string,
@@ -11,13 +13,21 @@ interface Props {
     isLight?: boolean
 }
 
-const TokenCard = ({className, ticker, icon, url, bgColor, isLight}: Props) => {
+const TokenCard = ({className, asset, ticker, icon, url, bgColor, isLight}: Props) => {
     return (
-        <Link href={url} target='_blank' className={`flex items-center justify-center h-20 sm:h-36 w-full sm:w-40 rounded-lg group overflow-hidden relative cursor-pointer ${bgColor} ${!isLight ? 'text-text-white' : 'text-text-black'} ${className}`}>
-            <div className='flex gap-2 items-center justify-center group-hover:opacity-0 duration-100'>
-                <Image src={icon} width={25} height={25} alt={ticker + " icon"} className='md:w-fit w-8'></Image>
-                <p className='md:text-2xl font-medium text-lg'>{ticker}</p>
+        <Link href={url} target='_blank' className={`flex items-center justify-center h-20 sm:h-40 w-full sm:w-44 rounded-lg group overflow-hidden relative cursor-pointer ${bgColor} ${!isLight ? 'text-text-white' : 'text-text-black'} ${className}`}>
+            <div className='flex flex-col items-center gap-2 group-hover:opacity-0 duration-100'>
+                <div className='flex gap-2 items-center justify-center'>
+                    <Image src={icon} width={25} height={25} alt={asset + " icon"} className='md:w-fit w-8'></Image>
+                    <p className='md:text-2xl font-medium text-lg'>{asset}</p>
+                </div>
+                <div className='grid  text-center text-sm' >
+                    <p>Tokenized as</p>
+                    <p>{ticker}</p>
+                </div>
+                
             </div>
+
             <div className='absolute -bottom-5 sm:group-hover:bottom-[3.75rem] group-hover:bottom-7 transition-all duration-300 ease-in-out'>
                 <p className='font-medium'>More Info ↗</p>
             </div>
