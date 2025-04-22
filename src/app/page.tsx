eca6cf04aa55eb25cc5b4e2a751a1d83dd444d58
Nicolas Taddeo <94655836+colotaddeo@users.noreply.github.com>
2025-04-22 15:10:28 -0300

    fix: grant section import deleted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import HeroSection from "@/components/sections/hero-section";
 import BenefitsSection from "@/components/sections/benefits-section";
 import FinalCtaSection from "@/components/sections/final-cta-section";
-import GrantSection from "@/components/sections/grant-support-section";
 import RocketSection from "@/components/sections/rocket-section";
 import StocksSection from "@/components/sections/stocks-section";
 import InvesorsSection from "@/components/sections/Investors";
