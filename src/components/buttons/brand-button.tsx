695898c855444e895d2beeebf8e9d0d96361c1b5
Nicolas Taddeo <94655836+colotaddeo@users.noreply.github.com>
2025-04-28 22:08:14 -0300

    fix:card update

--- src/components/buttons/brand-button.tsx
+++ src/components/buttons/brand-button.tsx
@@ -1,17 +1,17 @@
 import Link from "next/link";
 
 export const BrandButton = ({ children }: { children: React.ReactNode }) => {
-    return (
-      <button className="transition-all duration-500 whitespace-nowrap bg-accent hover:shadow-lg hover:shadow-blue-500/20 rounded-full px-4 text-sm h-12 font-medium text-foreground-heading flex items-center justify-center">
-        {children}
-      </button>
-    );
-  };
+  return (
+    <button className="transition-all duration-500 whitespace-nowrap bg-accent hover:shadow-lg hover:shadow-blue-500/20 rounded-full px-4 text-sm h-12 font-medium text-foreground-heading flex items-center justify-center">
+      {children}
+    </button>
+  );
+};
 
 export const BrandLink = ({ children, href }: { children: React.ReactNode, href: string }) => {
-    return (
-      <Link href={href} className="transition-all duration-500 whitespace-nowrap bg-accent hover:shadow-lg hover:shadow-blue-500/20 rounded-full px-5 text-sm h-12 font-medium text-foreground-heading flex items-center justify-center">
-        {children}
-      </Link>
-    );
-  };
+  return (
+    <Link href={href} className="transition-all duration-500 whitespace-nowrap bg-accent hover:shadow-lg hover:shadow-blue-500/20 rounded-full px-5 text-sm h-12 font-medium text-foreground-heading flex items-center justify-center">
+      {children}
+    </Link>
+  );
+};
--- src/components/cards/por-card.tsx
+++ src/components/cards/por-card.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Chevron from "../../../public/assets//icons/chevron.svg";
+import { BrandLink } from "../buttons/brand-button";
 
 interface Props {
     token: string;
@@ -9,49 +10,55 @@ interface Props {
 
 const PorCard = ({token, imageRoute}: Props) => {   
     return (
-        <div className="flex flex-col  gap-4 border rounded-md shadow-md px-10 py-6 bg-background">
-            <div className="flex flex-col gap-4 items-center justify-center">
-                <div className="flex flex-row items-center justify-between gap-x-6">
-                    <div>
-                        <h1 className="text-foreground-heading 2xl:text-[44px] sm:text-[50px] text-[34px] font-atyp-display font-medium leading-[120%]">{token}</h1>
-                        <h3 className="text-foreground-leading text-xl font-medium">Reserve ratio</h3>
+        <div>
+            <div className="flex flex-col gap-9 border rounded-md shadow-md px-10 py-6 bg-background">
+                <div className="flex flex-col justify-between gap-y-3">
+                    <div className="flex flex-row items-center justify-between gap-x-6">
+                        <div>
+                            <h1 className="text-foreground-heading 2xl:text-[36px] sm:text-[50px] text-[34px] font-atyp-display font-medium leading-[120%]">{token}</h1>
+                            <h3 className="text-foreground-leading text-xl font-medium">Reserve ratio</h3>
+                        </div>
+                        <Image src={imageRoute} alt="Token image" width={80} height={80}></Image>
                     </div>
-                    <Image src={imageRoute} alt="Token image" width={80} height={80}></Image>
-                </div>
-                <h1 className="text-foreground-heading 2xl:text-[32px] sm:text-[50px] text-[34px] font-atyp-display font-medium leading-[120%]">100%</h1>
-            </div>
-            <div>
-                <div className="flex flex-row items-center justify-between gap-x-6">
-                    <p>Collateral in custody</p>
-                    <p>1234</p>
-                </div>
-                <div className="flex flex-row items-center justify-between gap-x-6">
-                    <div className="flex flex-row items-center justify-start gap-x-1">
-                        <Image src={Chevron} width={12} height={12}></Image>
-                        <p>Circulating Tokens</p>
-                    </div>
-                    <p>1234</p>
-                </div>
-                <div className="flex flex-row items-center justify-between gap-x-6 ms-4">
-                    <p>Etherum</p>
-                    <p>1234</p>
+                    <h1 className="text-foreground-heading 2xl:text-[32px] sm:text-[50px] text-[34px] font-atyp-display font-medium leading-[120%]">100%</h1>
                 </div>
                 <div>
-                    <div className="flex flex-row items-center justify-between gap-x-6 ms-4">
-                        <p>Base</p>
-                        <p>1234</p>
+                    <div className="flex flex-row items-center justify-between gap-x-6">
+                        <p>Collateral in custody</p>
+                        <p className="text-xl font-medium">618.563,895</p>
                     </div>
-                    <div className="flex flex-row items-center justify-between gap-x-6 ms-4">
-                        <p>Optimism</p>
-                        <p>1234</p>
+
+                    <div className="flex flex-row items-center justify-between gap-x-6">
+                        <div className="flex flex-row items-center justify-start gap-x-1">
+                            <Image src={Chevron} width={12} height={12} alt="chevron down icon"></Image>
+                            <p>Circulating Tokens</p>
+                        </div>
+                        <p className="text-xl font-medium">618.563,895</p>
                     </div>
-                    <div className="flex flex-row items-center justify-between gap-x-6 ms-4">
-                        <p>Arbitrum One</p>
-                        <p>1234</p>
+
+                    <div className="gap-y-1 flex flex-col">    
+                        <div className="flex flex-row items-center justify-between gap-x-6 ms-4">
+                            <p className="underline text-[#245BFF]">Etherum</p>
+                            <p>154640,973</p>
+                        </div>
+                        <div className="flex flex-row items-center justify-between gap-x-6 ms-4">
+                            <p className="underline text-[#245BFF]">Base</p>
+                            <p>154640,973</p>
+                        </div>
+                        <div className="flex flex-row items-center justify-between gap-x-6 ms-4">
+                            <p className="underline text-[#245BFF]">Optimism</p>
+                            <p>154640,973</p>
+                        </div>
+                        <div className="flex flex-row items-center justify-between gap-x-6 ms-4">
+                            <p className="underline text-[#245BFF]">Arbitrum One</p>
+                            <p>154640,973</p>
+                        </div>
                     </div>
                 </div>
+                <BrandLink
+                href="https://app.bouldertech.fi/"
+                >View in CoinGecko</BrandLink>
             </div>
-            <button>view in Coingecko</button>
             
         </div>
     );
