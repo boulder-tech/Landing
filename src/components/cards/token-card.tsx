e2649ee59a272467a816ead3f2f348a221b20dc7
Nicolas Taddeo <94655836+colotaddeo@users.noreply.github.com>
2025-08-26 00:49:11 -0300

    fix: hover animation in deal card

--- src/components/cards/token-card.tsx
+++ src/components/cards/token-card.tsx
@@ -15,13 +15,13 @@ const TokenCard = ({name, description, icon, url, isComingSoon}: Props) => {
         <Link href={url} target='_blank' className={`flex items-top pt-12 h-56 p-4 justify-start sm:h-60 sm:w-60 md:h-60 md:w-60 rounded-lg group ${!isComingSoon ? 'overflow-hidden' : 'overflow-vissible'} relative cursor-pointer bg-[#FFFF] hover:shadow-2xl hover:shadow-[#0C32CC]/20 duration-300 ease-in-out`}>
             <div className={`flex flex-col items-center ${!isComingSoon ? 'group-hover:opacity-0 duration-100' : 'visible'}`}>
                 <div className='flex flex-col gap-2 items-center justify-center text-black'>
-                    <Image src={icon} width={40} height={40} alt={name + " icon"} className='md:w-8 w-8'></Image>
+                    <Image src={icon} width={40} height={40} alt={name + " icon"} className='w-8'></Image>
                     <p className='md:text-2xl md:w-56 font-medium text-lg text-center w-48'>{name}</p>
                     <p className='text-center w-52'>{description}</p>
                 </div>  
             </div>
 
-            <div className={`absolute -bottom-5 sm:group-hover:bottom-[6rem] group-hover:bottom-7 transition-all duration-300 ease-in-out ${!isComingSoon ? 'visible' : 'invisible'}`}>
+            <div className={`absolute -bottom-5 left-16 sm:group-hover:bottom-[6rem] justify-center group-hover:bottom-24 group-hover:left-16 transition-all duration-300 ease-in-out ${!isComingSoon ? 'visible' : 'invisible'}`}>
                 <p className='font-medium text-black'>Explore deal ↗</p>
             </div>
             <div className={`absolute top-[-16px] left-40 bg-[#E8EEFF] border-2 border-[#B3C4FF] whitespace-nowrap rotate-12 rounded-full p-1 ${!isComingSoon ? 'invisible' : 'visible'}`}>
